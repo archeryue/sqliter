01ef1a5df4c041767ab7a3e4c9330f690aa9034d
archeryue <archeryue7@gmail.com>
2023-04-21 22:47:07 +0800

    MOD: extract functions by copilot

--- table.cpp
+++ table.cpp
@@ -11,26 +11,37 @@ DataType Column::Type() const {
     return this->type;
 }
 
-DataType Str2Type(std::string_view str) {
+DataType str2Type(std::string_view str) {
     return str == "int" ? INT : str == "double" ? DOUBLE : CHAR32;
 }
 
-std::vector<Column> ParseColumn(std::string_view column_def) {
-    std::vector<std::string_view> col_str;
+std::vector<std::string_view> split(std::string_view str, char delim) {
+    std::vector<std::string_view> res;
     std::size_t start = 0;
-    while (start < column_def.size()) {
-        auto end = column_def.find_first_of(',', start);
-        if (start != end) col_str.emplace_back(column_def.substr(start, end-start));
+    while (start < str.size()) {
+        auto end = str.find_first_of(delim, start);
+        if (start != end) res.emplace_back(str.substr(start, end-start));
         if (end == std::string_view::npos) break;
         start = end + 1;
     }
+    return res;
+}
+
+// extract column name and type from string
+// e.g. "id:int,name:char32,age:int"
+Column str2Column(std::string_view column_def) {
+    int idx = column_def.find(':');
+    std::string name(column_def.substr(0, idx));
+    DataType type = str2Type(column_def.substr(idx+1, column_def.size()+1));
+    Column col(name, type);
+    return col;
+}
+
+std::vector<Column> ParseColumn(std::string_view column_def) {
+    std::vector<std::string_view> col_str = split(column_def, ',');
     std::vector<Column> columns;
     for (auto& str : col_str) {
-        int idx = str.find(':');
-        std::string name(str.substr(0, idx));
-        DataType type = Str2Type(str.substr(idx+1, str.size()+1));
-        Column col(name, type);
-        columns.push_back(col);
+        columns.push_back(str2Column(str));
     }
     return columns;
 }
